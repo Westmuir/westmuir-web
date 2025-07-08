41380cea168c34c09b9bc7474fb2562929250b46
philip ross <philip@satchel.scot>
2025-07-08 13:18:37 +0100

    fix: element references in nav toggle

--- src/scripts/nav-toggle.js
+++ src/scripts/nav-toggle.js
@@ -1,70 +1,35 @@
 let openNavButton;
 
 document.addEventListener('DOMContentLoaded', () => {
-  // const toggleButton = document.getElementById('nav-toggle');
-  // const navMenu = document.getElementById('nav-menu');
+  const navbar = document.querySelector('#site-nav');
+  const openNavButton = document.getElementById('open-nav-button');
 
-  // if (toggleButton && navMenu) {
-  //   toggleButton.addEventListener('click', () => {
-  //     navMenu.classList.add(open);
-  //   });
-
-  //   document.addEventListener('click', e => {
-  //     if (!navMenu.contains(e.target) && !toggleButton.contains(e.target)) {
-  //       navMenu.classList.remove('open');
-  //     }
-  //   });
-  // }
-
-  const media = window.matchMedia('(width < 700px)');
-
-  media.addEventListener('change', e => updateNavbar(e));
-
-  openNavButton = document.getElementById('open-nav-button');
-
-  if (openNavButton) {
-    openNavButton.addEventListener('click', () => {
-      openSidebar();
-    });
-  }
-
-  const closeNavButton = document.getElementById('close-nav-button');
-
-  if (closeNavButton) {
-    closeNavButton.addEventListener('click', () => {
-      closeSidebar();
-    });
-  }
-
-  const overlay = document.getElementById('overlay');
-
-  if (overlay) {
-    overlay.addEventListener('click', e => {
-      closeSidebar();
-    });
-  }
-
-  updateNavbar(media);
-});
-
-function updateNavbar(e) {
-  const isMobile = e.matches;
-  if (isMobile) {
+  const closeSidebar = () => {
+    navbar.classList.remove('show');
+    openNavButton.setAttribute('aria-expanded', 'false');
     navbar.setAttribute('inert', '');
-  } else {
-    // desktop device
+  };
+
+  const updateNavbar = e => {
+    // if matches then implies isMobile,  so set to be inert
+    e?.matches ? navbar.setAttribute('inert', '') : navbar.removeAttribute('inert');
+  };
+
+  window.matchMedia('(width < 700px)').addEventListener('change', e => updateNavbar(e));
+
+  openNavButton?.addEventListener('click', () => {
+    navbar.classList.add('show');
+    openNavButton.setAttribute('aria-expanded', 'true');
     navbar.removeAttribute('inert');
-  }
-}
+  });
 
-function openSidebar() {
-  navbar.classList.add('show');
-  openNavButton.setAttribute('aria-expanded', 'true');
-  navbar.removeAttribute('inert');
-}
+  document.getElementById('close-nav-button')?.addEventListener('click', () => {
+    closeSidebar();
+  });
 
-function closeSidebar() {
-  navbar.classList.remove('show');
-  openNavButton.setAttribute('aria-expanded', 'false');
-  navbar.setAttribute('inert', '');
-}
+  document.getElementById('overlay')?.addEventListener('click', e => {
+    closeSidebar();
+  });
+
+  updateNavbar();
+});
